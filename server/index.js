d6096c4e9c2dd28cfa9db403f03f48d7c47c1ff4
ivan <ivanmantello@gmail.com>
2023-09-19 12:53:09 -0300

    cambios

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const axios = require("axios");
 const server = require("./src/server");
 const { conn } = require('./src/db.js');
-const PORT = 3001;
+
+require('dotenv').config()
+
+// const PORT = 3001;
 
 conn.sync({ force: true }).then(() => {
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+server.listen(process.env.PORT, () => {
+  console.log(`Server listening on port `, process.env.PORT);
 })
 }).catch(error => console.error(error))
